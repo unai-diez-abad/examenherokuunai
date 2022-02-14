4087f190227f65ae3c5ef8ec242c09c276274aac
unknown <2gdaw06@arriagainfo.local>
2022-02-14 12:06:59 +0100

    base de datos

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -2,15 +2,16 @@
 class Database {
 	public static $db;
 	public static $con;
+	mysql://b727a069b4f4e0:dd01c150@us-cdbr-east-05.cleardb.net/heroku_3686daf2efe4b77?reconnect=true
 	function Database(){
-		$this->user="b61c5915ab24eb";$this->pass="9cc656bd";$this->host="eu-cdbr-west-02.cleardb.net";$this->ddbb="heroku_a01bc4777651db0";
+		$this->user="b727a069b4f4e0";$this->pass="dd01c150";$this->host="us-cdbr-east-05.cleardb.net";$this->ddbb="heroku_3686daf2efe4b77";
 		//Copiar también la información de la conexión en la función de abajo connect()
 	}
 
 	function connect(){
 		//Copiar aquí también la info de la conexión
-		$this->user="b61c5915ab24eb";$this->pass="9cc656bd";$this->host="eu-cdbr-west-02.cleardb.net";$this->ddbb="heroku_a01bc4777651db0";
-		$con = new mysqli($this->host,$this->user,$this->pass,$this->ddbb);
+		$this->user="b727a069b4f4e0";$this->pass="dd01c150";$this->host="us-cdbr-east-05.cleardb.net";$this->ddbb="heroku_3686daf2efe4b77";
+		$con = new mysqli($this->host="us-cdbr-east-05.cleardb.net",$this->user="b727a069b4f4e0",$this->pass="dd01c150",$this->ddbb="heroku_3686daf2efe4b77");
 		$con->query("set sql_mode=''");
 		return $con;
 	}
